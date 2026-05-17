9fe841fadc7b736cf77d8770722ba448e15df20f
sri satya sai <srisatyasai136@gmail.com>
2026-05-17 17:41:35 +0530

    Update captcha implementation in Loginform
    Replaced ReCaptchaField with django_recaptcha implementation and updated the widget to ReCaptchaV2Checkbox.

--- login/forms.py
+++ login/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from captcha.fields import ReCaptchaField
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 class Loginform(forms.Form):
     username=forms.EmailField(max_length=100)
     password=forms.CharField(widget=forms.PasswordInput)
     # googlecaptcha=ReCaptchaField()
+    googlecaptcha=ReCaptchaField(
+        widget=ReCaptchaV2Checkbox
+    )
     captcha=forms.CharField(max_length=5, widget=forms.TextInput(attrs={'class':'captcha-input','placeholder':'Enter code'}))
 
