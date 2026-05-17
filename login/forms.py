5ffaf0455a5301145552f4bc483d30b5712d0905
sri satya sai <srisatyasai136@gmail.com>
2026-05-17 13:22:47 +0530

    Disable googlecaptcha field in Loginform
    Comment out the googlecaptcha field in Loginform.

--- login/forms.py
+++ login/forms.py
@@ -3,6 +3,6 @@ from captcha.fields import ReCaptchaField
 class Loginform(forms.Form):
     username=forms.EmailField(max_length=100)
     password=forms.CharField(widget=forms.PasswordInput)
-    googlecaptcha=ReCaptchaField()
+    # googlecaptcha=ReCaptchaField()
     captcha=forms.CharField(max_length=5, widget=forms.TextInput(attrs={'class':'captcha-input','placeholder':'Enter code'}))
 
